7b51af636087d2767bafecfbca155cbcdcac774a
PSJay <psjay.peng@gmail.com>
2013-10-16 23:43:36 +0800

    Finished baidu login.

--- songync/clients.py
+++ songync/clients.py
@@ -7,20 +7,30 @@ Clients for supported music site
 
 import logging
 import re
+from collections import namedtuple
 
 import requests
 from requests.exceptions import RequestException
 
+from songync.decorators import retry
 from songync.exceptions import AuthException
-from songync.utils import retry
+
+
+SongInfo = namedtuple("SongInfo", ['name', 'artist', 'album'])
 
 
 class BaseClient(object):
 
     def __init__(self):
         self.session = requests.Session()
+        self._login_flag = False
 
     def login(self, username, password):
+        if self.do_login(username, password):
+            self._login_flag = True
+        return self._login_flag
+
+    def do_login(self, username, password):
         raise NotImplementedError('This method has not been implemented.')
 
     def get_fav_songs_info(self):
@@ -37,13 +47,16 @@ class BaiduMusicClient(BaseClient):
 
     _LOGIN_TOKEN_URL = 'https://passport.baidu.com/v2/api/?getapi&class=login&tpl=music&tangram=true'
     _LOGIN_URL = 'https://passport.baidu.com/v2/api/?login'
+    _SONG_IDS_URL = 'http://yinyueyun.baidu.com/data/cloud/collection?type=song'
+    _SONG_INFO_URL = 'http://yinyueyun.baidu.com/data/cloud/songinfo'
+    _CAPTCHA_URL = 'https://passport.baidu.com/cgi-bin/genimage'
 
-    def login(self, username, password):
+    def do_login(self, username, password):
         login_token = None
         with retry(3, RequestException):
             self.session.get('http://yinyueyun.baidu.com')
             r = self.session.get(self._LOGIN_TOKEN_URL)
-            if not r.status_code == 200:
+            if r.status_code != 200:
                 logging.error('status code: %d' % r.status_code)
 
             text = r.text
@@ -54,7 +67,7 @@ class BaiduMusicClient(BaseClient):
             if not m:
                 logging.error('Could not get login token.')
                 logging.error(text)
-                raise
+                return False
             login_token = m.group(1)
 
         login_data = {
@@ -74,15 +87,74 @@ class BaiduMusicClient(BaseClient):
         }
         with retry(3, RequestException):
             r = self.session.post(self._LOGIN_URL, data=login_data)
-            if not r.status_code == 200:
+            if r.status_code != 200:
                 logging.error('status code: %d' % r.status_code)
+
+            while True:
+                m = re.search(r'&error=(\d+)', r.text)
+                if not (m and int(m.group(1)) in [257, 6]):
+                    break
+                # need verify
+                code_str_m = re.search(r'codestring=(\w+)', r.text)
+                codestring = code_str_m.group(1) if code_str_m else ''
+                if not codestring:
+                    logging.error('Could not get the codestring for get captcha url.')
+                    return False
+                print 'Need verifying, please open the following url in your browser then input the catpcha you will see below before any refreshing:'
+                print self._CAPTCHA_URL + '?' + codestring
+                verifycode = raw_input('verify code: ')
+
+                login_data['verifycode'] = verifycode
+                login_data['codestring'] = codestring
+
+                with retry(3, RequestException):
+                    r = self.session.post(self._LOGIN_URL, data=login_data)
+                    if r.status_code != 200:
+                        logging.error('status code: %d' % r.status_code)
+
             if 'BDUSS' not in r.cookies:
                 raise AuthException('Baidu music Auth failed. Reponse content:\n %s' % r.text)
 
-            print 'Login successfully.'
+            print 'Login Baidu Music successfully.'
+            return True
 
     def get_fav_songs_info(self):
-        raise NotImplementedError('This method has not been implemented.')
+        batch_size = 200
+        infos = []
+        total = 500
+        start = 0
+        while start < total:
+            tmp_start = start
+            start += batch_size
+            song_ids = []
+            with retry(3, RequestException):
+                r = self.session.get(self._SONG_IDS_URL, params={
+                    'start': tmp_start,
+                    'size': batch_size
+                })
+                if r.status_code != 200:
+                    logging.error('status code: %d' % r.status_code)
+                    continue
+                res = r.json()
+                if res['errorCode'] == 22000:
+                    total = res['data']['total']
+                    song_list = res['data']['songList']
+                    song_ids = [str(s['id']) for s in song_list]
+            if not song_ids:
+                continue
+            with retry(3, RequestException):
+                r = self.session.post('http://yinyueyun.baidu.com/data/cloud/songinfo', data={
+                    'songIds': ','.join(song_ids)
+                })
+                if not r.status_code == 200:
+                    logging.error('status code: %d' % r.status_code)
+                    continue
+                res = r.json()
+                if res['errorCode'] == 22000:
+                    song_list = res['data']['songList']
+                    song_infos = [SongInfo(s['songName'], s['artistName'], s['albumName']) for s in song_list]
+                    infos.extend(song_infos)
+        return infos
 
 
 class DoubanFMClient(BaseClient):
